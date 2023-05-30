e1b48f450f656825efffb13faa17fcdd162be1b7
namu2267 <namu2267@gmail.com>
2023-05-31 03:39:06 +0900

    mbtiDetilpage:파라미터기준으로 정적인 데이터받아와서 화면에띄워주기

--- src/pages/MbtiDetailPage.tsx
+++ src/pages/MbtiDetailPage.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { db } from '../firebase';
+import { mbtiEach } from '../fixtures/MbtiEach';
 
 export default function MbtiDetailPage() {
-  return <div>엠비티아이 디테일페이지</div>;
+  const { id } = useParams<{ id: string }>();
+
+  // 해당 ID에 해당하는 데이터 찾기
+  const mbtiFixData = mbtiEach.find((item) => item.id === Number(id));
+
+  return (
+    <div>
+      <div>엠비티아이 디테일페이지</div>
+      <div>ID: {id}</div>
+      <div>{mbtiFixData.title}</div>
+      <img alt="mbtiPhoto" src={mbtiFixData.src} />
+    </div>
+  );
 }
